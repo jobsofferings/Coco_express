05e4423666b706ab2e1758ef1da954dce03a12b7
jobs <2821740092@qq.com>
2021-03-18 20:57:08 +0800

    修改CORS

--- app/app.ts
+++ app/app.ts
@@ -9,7 +9,7 @@ const app: express.Application = express();
 const port = 5000;
 
 app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin','http://localhost:3000'); //当允许携带cookies此处的白名单不能写’*’
+  res.header('Access-Control-Allow-Origin','http://localhost:3000', 'http://www.jobsofferings.cn'); //当允许携带cookies此处的白名单不能写’*’
   res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
   next();
